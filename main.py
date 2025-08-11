2828db1a300ecb20883f49ca786536917d16af33
Nikitoxx <nekithis@gmail.com>
2025-08-11 15:12:46 +0300

    Made a function encryption in Morse code

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('600x300+800+100')
+root.geometry('550x150+800+100')
 root.configure(bg='Black')
+
+# Code morze
+morse_code = {
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',
+    'E': '.',     'F': '..-.',  'G': '--.',   'H': '....',
+    'I': '..',    'J': '.---',  'K': '-.-',   'L': '.-..',
+    'M': '--',    'N': '-.',    'O': '---',   'P': '.--.',
+    'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+    'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',
+    'Y': '-.--',  'Z': '--..',
+
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.',
+
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.',
+    '!': '-.-.--', '/': '-..-.',  '(': '-.--.',  ')': '-.--.-',
+    '&': '.-...',  ':': '---...', ';': '-.-.-.',
+    '=': '-...-',  '+': '.-.-.',  '-': '-....-', '_': '..--.-',
+    '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+}
+
+
+
+#Functions 
+
+def converte():
+    a = ent.get()
+    global morse_code
+    result = ' '.join([morse_code[x] for x in a.upper() if x != ' '])
+    lab['text'] = result
+    
+def clear():
+    ent.delete(0, END)
+    lab['text'] = ''
 
 
 # Interface
@@ -10,13 +45,20 @@ root.configure(bg='Black')
 ent = Entry(font=('Arial', 20), width=30)
 ent.grid(column=0, row=0, padx=10, pady=10)
 
-btn1 = Button(font=('Arial', 15), text= 'Enter', bg='orange' )
+btn1 = Button(font=('Arial', 15), text= 'Enter', bg='orange', command=converte)
 btn1.grid(column=1, row=0)
-btn1 = Button(font=('Arial', 15), text= 'Clear', bg='orange' )
+btn1 = Button(font=('Arial', 15), text= 'Clear', bg='orange', command=clear)
 btn1.grid(column=1, row=1)
 
-lab = Label(font=('Arial', 20), width=28, text='')
+lab = Label(font=('Arial', 20), width=28, text='', anchor='w')
 lab.grid(column=0, row=1, padx=10, pady=10)
+
+
+
+    
+        
+        
+        
 
 
 
