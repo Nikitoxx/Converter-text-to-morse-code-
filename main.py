a29e5c60cc4e8a1f299c84e9a945fefc22628b90
Nikitoxx <nekithis@gmail.com>
2025-08-11 14:30:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('600x300+800+100')
+root.configure(bg='Black')
+
+ent = Entry(font=('Arial', 20), width=30)
+ent.grid(column=0, row=0, padx=10, pady=10)
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
